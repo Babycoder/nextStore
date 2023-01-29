fef0c518f487aea63bedc5bfe677ee277f2b4223
Ghazali <aymenghazali1999@gmail.com>
2023-01-29 16:13:47 +0100

    navbar setup

--- components/Layout.js
+++ components/Layout.js
@@ -1,29 +1,33 @@
-import React from "react"
-import Head from 'next/head'
+import React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
-
-export default function Layout({title, children}) {
+export default function Layout({ title, children }) {
   return (
     <>
-        <Head>
-            <title>{title ? title + 'Store' : 'nextStore'}</title>
-            <meta name="description" content="Ecommerce website" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        
-        <div className="flex min-h-screen flex-col justify-between">
-            <header>
-                header
-            </header>
-            
-            <main>
-                {children}
-            </main>
-            
-            <footer>
-                footer
-            </footer>
-        </div>
+      <Head>
+        <title>{title ? title + 'Store' : 'nextStore'}</title>
+        <meta name="description" content="Ecommerce website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="px-4 flex h-12 justify-between shadow-md hover:shadow-lg items-center">
+            <Link href="/">
+              <a className="text-lg font-bold">nextStore</a>
+            </Link>
+            <div className='text-base'>
+              <Link href="/cart"><a className='p-2'>Cart</a></Link>
+              <Link href="/login"><a className='p-2'>Login</a></Link>
+            </div>
+          </nav>
+        </header>
+
+        <main>{children}</main>
+
+        <footer>footer</footer>
+      </div>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Layout from '../components/Layout'
-
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    
-      <Layout>content</Layout>
-  )
+  return <Layout>content</Layout>;
 }
-
