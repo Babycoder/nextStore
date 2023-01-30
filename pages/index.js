be2263c5541de7dcf32b2039c2bfcf0bf56e2743
Ghazali <aymenghazali1999@gmail.com>
2023-01-30 11:34:48 +0100

    product item

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import data from '../utils/data';
 import Layout from '../components/Layout';
+import Productitem from '../components/Productitem';
 
 export default function Home() {
-  return <Layout>Home page</Layout>;
+  return (
+    <Layout>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <Productitem product={product} key={product.slug}></Productitem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
